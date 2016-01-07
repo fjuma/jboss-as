42aa15854b3f4473cb1f2571b84d8b9cb5deb441
Farah Juma <fjuma@redhat.com>
2016-01-06 19:03:25 -0500

    [WFLY-5689] Add a runtime attribute that lists the deployments making use of a security domain mapping

--- ejb3/src/main/java/org/jboss/as/ejb3/subsystem/EJB3SubsystemModel.java
+++ ejb3/src/main/java/org/jboss/as/ejb3/subsystem/EJB3SubsystemModel.java
@@ -139,6 +139,7 @@ public interface EJB3SubsystemModel {
 
     // Elytron integration
     String APPLICATION_SECURITY_DOMAIN = "application-security-domain";
+    String REFERENCING_DEPLOYMENTS = "referencing-deployments";
     String SECURITY_DOMAIN = "security-domain";
     String SECURITY_DOMAIN_REF = "security-domain-ref";
 }
