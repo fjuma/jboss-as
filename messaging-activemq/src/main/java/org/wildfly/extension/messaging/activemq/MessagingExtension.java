5d2472782c737febcc579183eedf1f27c4390ca1
Farah Juma <fjuma@redhat.com>
2019-08-14 17:51:39 -0400

    [WFLY-12218] Bump messaging schema version to 8.0

--- messaging-activemq/src/main/java/org/wildfly/extension/messaging/activemq/MessagingExtension.java
+++ messaging-activemq/src/main/java/org/wildfly/extension/messaging/activemq/MessagingExtension.java
@@ -98,7 +98,14 @@ import org.wildfly.extension.messaging.activemq.jms.ExternalPooledConnectionFact
  * Domain extension that integrates Apache ActiveMQ 6.
  *
  * <dl>
- * <dt><strong>Current</strong> - WildFly 17</dt>
+ * <dt><strong>Current</strong> - WildFly 18</dt>
+ *   <dd>
+ *     <ul>
+ *       <li>XML namespace: urn:jboss:domain:messaging-activemq:8.0
+ *       <li>Management model: 8.0.0
+ *     </ul>
+ *   </dd>
+ * <dt>WildFly 17</dt>
  *   <dd>
  *     <ul>
  *       <li>XML namespace: urn:jboss:domain:messaging-activemq:7.0
@@ -202,6 +209,7 @@ public class MessagingExtension implements Extension {
 
     static final String RESOURCE_NAME = MessagingExtension.class.getPackage().getName() + ".LocalDescriptions";
 
+    protected static final ModelVersion VERSION_8_0_0 = ModelVersion.create(8, 0, 0);
     protected static final ModelVersion VERSION_7_0_0 = ModelVersion.create(7, 0, 0);
     protected static final ModelVersion VERSION_6_0_0 = ModelVersion.create(6, 0, 0);
     protected static final ModelVersion VERSION_5_0_0 = ModelVersion.create(5, 0, 0);
@@ -209,9 +217,9 @@ public class MessagingExtension implements Extension {
     protected static final ModelVersion VERSION_3_0_0 = ModelVersion.create(3, 0, 0);
     protected static final ModelVersion VERSION_2_0_0 = ModelVersion.create(2, 0, 0);
     protected static final ModelVersion VERSION_1_0_0 = ModelVersion.create(1, 0, 0);
-    private static final ModelVersion CURRENT_MODEL_VERSION = VERSION_7_0_0;
+    private static final ModelVersion CURRENT_MODEL_VERSION = VERSION_8_0_0;
 
-    private static final MessagingSubsystemParser_7_0 CURRENT_PARSER = new MessagingSubsystemParser_7_0();
+    private static final MessagingSubsystemParser_8_0 CURRENT_PARSER = new MessagingSubsystemParser_8_0();
 
     // ARTEMIS-2273 introduced audit logging at a info level which is rather verbose. We need to use static loggers
     // to ensure the log levels are set to WARN and there is a strong reference to the loggers. This hack will likely
@@ -318,6 +326,7 @@ public class MessagingExtension implements Extension {
         context.setSubsystemXmlMapping(SUBSYSTEM_NAME, MessagingSubsystemParser_4_0.NAMESPACE, MessagingSubsystemParser_4_0::new);
         context.setSubsystemXmlMapping(SUBSYSTEM_NAME, MessagingSubsystemParser_5_0.NAMESPACE, MessagingSubsystemParser_5_0::new);
         context.setSubsystemXmlMapping(SUBSYSTEM_NAME, MessagingSubsystemParser_6_0.NAMESPACE, MessagingSubsystemParser_6_0::new);
-        context.setSubsystemXmlMapping(SUBSYSTEM_NAME, MessagingSubsystemParser_7_0.NAMESPACE, CURRENT_PARSER);
+        context.setSubsystemXmlMapping(SUBSYSTEM_NAME, MessagingSubsystemParser_7_0.NAMESPACE, MessagingSubsystemParser_7_0::new);
+        context.setSubsystemXmlMapping(SUBSYSTEM_NAME, MessagingSubsystemParser_8_0.NAMESPACE, CURRENT_PARSER);
     }
 }
